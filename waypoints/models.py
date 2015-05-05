33f74adcfcb45da10b4d794c4acee96ceb32b7e7
Nadine Jamila <nadinejamila@gmail.com>
2015-05-05 17:14:52 +0800

    Revert "add way point models and migrations"
    This reverts commit 3edb58c4ddb520291b9237c810de751cbe188ef1.

--- waypoints/models.py
+++ waypoints/models.py
@@ -1,10 +1,3 @@
-from django.contrib.gis.db import models
+from django.db import models
 
-
-class WayPoint(models.Model):
-	name = models.CharField(max_length=32)
-	geometry = models.PointField(srid=4326) #spatial reference
-	objects = models.GeoManager() #every GIS-based django model needs one of these for geospatial queries
-
-	def __unicode__(self):
-		return '%s %s %s' % (self.name, self.geometry.x, self.geometry.y)
+# Create your models here.
