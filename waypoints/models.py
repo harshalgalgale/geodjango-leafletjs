d5d9e3eba7029b0c0de4a8a1d490c7b32ae0454a
Nadine Jamila <nadinejamila@gmail.com>
2015-05-05 17:20:10 +0800

    added waypoint model and migrations

--- waypoints/models.py
+++ waypoints/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class WayPoint(models.Model):
+    name = models.CharField(max_length=32)
+    geometry = models.PointField(srid=4326) #spatial reference
+    objects = models.GeoManager() #every GIS-based django model needs one of these for geospatial queries
+
+    def __unicode__(self):
+        return '%s %s %s' % (self.name, self.geometry.x, self.geometry.y)
